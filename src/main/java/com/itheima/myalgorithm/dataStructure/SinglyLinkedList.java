487f83b2005ff2df8e51325cf7b1aeb106faba45
李鸿儒 <1256552047@qq.com>
2024-06-20 09:40:16 +0800

    单向链表初级功能添加

--- src/main/java/com/itheima/myalgorithm/dataStructure/SinglyLinkedList.java
+++ src/main/java/com/itheima/myalgorithm/dataStructure/SinglyLinkedList.java
@@ -10,118 +10,159 @@ import java.util.function.Consumer;
  * @Version 1.0
  */
 public class SinglyLinkedList implements Iterable<Integer> { // 整体
-    // 头指针
-    private Node head = null;
+	// 头指针
+	private Node head = null;
 
-    /**
-     * 遍历器
-     * 
-     * @return
-     */
-    @Override
-    public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
-            // 指针变量
-            SinglyLinkedList.Node p = head;
+	/**
+	 * 遍历器
+	 *
+	 * @return
+	 */
+	@Override
+	public Iterator<Integer> iterator() {
+		return new Iterator<Integer>() {
+			// 指针变量
+			SinglyLinkedList.Node p = head;
 
-            @Override
-            public boolean hasNext() { // 查看是否有下一个元素
-                return p != null;
-            }
+			@Override
+			public boolean hasNext() { // 查看是否有下一个元素
+				return p != null;
+			}
 
-            @Override
-            public Integer next() { // 返回当前元素，并且指针向下一个元素移动
-                int v = p.value;
-                p = p.next;
-                return v;
-            }
-        };
-    }
+			@Override
+			public Integer next() { // 返回当前元素，并且指针向下一个元素移动
+				int v = p.value;
+				p = p.next;
+				return v;
+			}
+		};
+	}
 
-    /**
-     * 向链表头部添加
-     * 
-     * @param value 添加元素的值
-     */
-    public void addFirst(int value) {
-        // 链表为空 or 非空
-        head = new Node(value, head);
-    }
+	/**
+	 * 向链表头部添加
+	 *
+	 * @param value 添加元素的值
+	 */
+	public void addFirst(int value) {
+		// 链表为空 or 非空
+		head = new Node(value, head);
+	}
 
-    /**
-     * 向链表尾部添加
-     * 
-     * @param value 添加元素的值
-     */
-    public void addLast(int value) {
-        Node last = findLast();
-        // 链表为空
-        if (findLast() == null) {
-            addFirst(value);
-            return;
-        }
-        // 链表非空
-        last.next = new Node(value, null);
-    }
+	/**
+	 * 向链表尾部添加
+	 *
+	 * @param value 添加元素的值
+	 */
+	public void addLast(int value) {
+		Node last = findLast();
+		// 链表为空
+		if (findLast() == null) {
+			addFirst(value);
+			return;
+		}
+		// 链表非空
+		last.next = new Node(value, null);
+	}
 
-    /**
-     * 寻找链表中最后一个元素
-     * 
-     * @return 若找到返回 Node , 链表为空返回 null
-     */
-    private Node findLast() {
-        // 1.链表为空
-        if (head == null) {
-            return head;
-        }
+	/**
+	 * 寻找链表中最后一个元素
+	 *
+	 * @return 若找到返回 Node , 链表为空返回 null
+	 */
+	private Node findLast() {
+		// 1.链表为空
+		if (head == null) {
+			return head;
+		}
 
-        // 2.链表非空
-        Node p;
-        for (p = head; p.next != null; p = p.next) {
+		// 2.链表非空
+		Node p;
+		for (p = head;
+				p.next != null;
+				p = p.next) {
+		}
+		return p;
+	}
 
-        }
-        return p;
-    }
+	/**
+	 * 遍历链表 Consumer
+	 *
+	 * @param consumer
+	 */
+	public void loop1(Consumer<Integer> consumer) {
+		// 遍历指针
+		Node p = head;
+		// 当指针没有指向最后一个元素的后一个 null 时继续遍历
+		while (p != null) {
+			consumer.accept(p.value);
+			p = p.next;
+		}
+	}
 
-    /**
-     * 遍历链表 Consumer
-     * 
-     * @param consumer
-     */
-    public void loop1(Consumer<Integer> consumer) {
-        // 遍历指针
-        Node p = head;
-        // 当指针没有指向最后一个元素的后一个 null 时继续遍历
-        while (p != null) {
-            consumer.accept(p.value);
-            p = p.next;
-        }
-    }
+	/**
+	 * 遍历链表 Consumer for循环
+	 *
+	 * @param consumer
+	 */
+	public void loop2(Consumer<Integer> consumer) {
+		for (Node p = head;
+				p != null;
+				p = p.next) {
+			consumer.accept(p.value);
+		}
+	}
 
-    /**
-     * 遍历链表 Consumer for循环
-     * 
-     * @param consumer
-     */
-    public void loop2(Consumer<Integer> consumer) {
-        for (Node p = head; p != null; p = p.next) {
-            consumer.accept(p.value);
-        }
-    }
+	/**
+	 * 节点类
+	 */
+	private static class Node {
+		/**
+		 * static 当内部类没有使用外部类的成员变量时，加上static
+		 */
+		int value; // 值
+		Node next; // 下一个节点指针
 
-    /**
-     * 节点类
-     */
-    private static class Node {
-        /**
-         * static 当内部类没有使用外部类的成员变量时，加上static
-         */
-        int value; // 值
-        Node next; // 下一个节点指针
+		public Node(int value, Node next) { // 有参构造器
+			this.value = value;
+			this.next = next;
+		}
+	}
 
-        public Node(int value, Node next) { // 有参构造器
-            this.value = value;
-            this.next = next;
-        }
-    }
+	/**
+	 * 通过索引寻找元素
+	 * @param index
+	 * @return
+	 */
+	private Node findNode(int index){
+		// 定义指针 , 索引起始点
+		Node p = head;
+		int i = 0;
+
+		for (; p.next != null; p = p.next, i++){
+			if (i == index){
+				return p;
+			}
+		}
+
+		// 没有找到
+		return null;
+	}
+
+	/**
+	 * 根据索引查找元素的值
+	 * @param index 查找索引
+	 * @return 元素的值，若未找到报错
+	 */
+	public int get(int index) {
+		Node node = findNode(index);
+		if (node == null) {
+			// 抛异常
+			throw new IllegalArgumentException(
+					String.format("index [%d] 不合法%n", index));
+		}
+		return node.value;
+
+	}
+
+
 }
--- src/test/java/com/itheima/myalgorithm/TestSinglyLinkedList.java
+++ src/test/java/com/itheima/myalgorithm/TestSinglyLinkedList.java
@@ -1,7 +1,6 @@
 package com.itheima.myalgorithm;
 
-
-import dataStructure.SinglyLinkedList;
+import com.itheima.myalgorithm.dataStructure.SinglyLinkedList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,47 +14,54 @@ import java.util.List;
  */
 public class TestSinglyLinkedList {
 
-    @Test
-    public void testLoop() {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.addFirst(1);
-        list.addFirst(2);
-        list.addFirst(3);
-        list.addFirst(4);
+	@Test
+	public void testLoop() {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.addFirst(1);
+		list.addFirst(2);
+		list.addFirst(3);
+		list.addFirst(4);
 
-        list.loop1(value -> {
-            System.out.println(value);
-        });
+		list.loop1(value -> {
+			System.out.println(value);
+		});
 
-        System.out.println("-------------");
+		System.out.println("-------------");
 
-        list.loop2(value -> {
-            System.out.println(value);
-        });
-    }
+		list.loop2(value -> {
+			System.out.println(value);
+		});
+	}
 
-    @Test
-    public void testForEachByIterable() {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.addFirst(1);
-        list.addFirst(2);
-        list.addFirst(3);
-        list.addFirst(4);
+	@Test
+	public void testForEachByIterable() {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.addFirst(1);
+		list.addFirst(2);
+		list.addFirst(3);
+		list.addFirst(4);
 
-        for (Integer value : list) {
-            System.out.println(value);
-        }
-    }
+		for (Integer value : list) {
+			System.out.println(value);
+		}
+	}
 
-    @Test
-    public void testAddLast() {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.addLast(1);
-        list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
+	@Test
+	public void testAddLast() {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.addLast(1);
+		list.addLast(2);
+		list.addLast(3); // index = 2
+		list.addLast(4);
 
-        Assertions.assertIterableEquals(List.of(1,2,3,4), list);
-    }
+		Assertions.assertIterableEquals(List.of(1, 2, 3, 4), list);
+
+		int i = list.get(2);
+		System.out.println(i == 3);
+
+		// int j = list.get(10); 索引不合法
+		// System.out.println(j);
+
+	}
 
 }
